ce13da75894ae3bbd8f8362f6e15cc306213dd35
Mario Alina <Epakfailer@gmail.com>
2021-03-12 23:12:41 +0100

    FixedRotation

--- Assets/Scripts/Tanks/PlayerTank.cs
+++ Assets/Scripts/Tanks/PlayerTank.cs
@@ -14,37 +14,41 @@ public class PlayerTank : TankBase
     private ProjectilePoolingScript poolingScript;
     [SerializeField]
     private bool isFiring = false;
-    
-    
+    private Rigidbody2D playerRb;
+    [SerializeField]
+    private float rotationSpeed = 5f;
+    private Vector3 rotationDirection;
+
+
+    private bool isFacingLeft = false;
+    private bool isFacingRight = false;
+    private bool isFacingUp = false;
+    private bool isFacingDown = false;
+
+
+
+
     private void Start()
     {
+        
         poolingScript = gameObject.GetComponent<ProjectilePoolingScript>();
-        facingDirection = new Vector3(0, 0, 0);
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
         fireDelay = 0.25f;
     }
 
 
     void Update()
     {
-        movementX = Input.GetAxis("Horizontal");
-        movementY = Input.GetAxis("Vertical");
-
-        SetRotationBasedOnMovement();
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            isFiring = true;
-        }
-        else if(Input.GetKeyUp(KeyCode.Space))
-        {
-            isFiring = false;
-        }
+        
+        GetPlayerInput();
     }
 
 
     void FixedUpdate()
     {
-        Move();
+        // reseting rotation direction, so we can correctly rotate to where we need to go
+        rotationDirection = new Vector3(0, 0, 0);
+        MovePlayer();
         Fire();
         fireElapsedTime += Time.deltaTime;
     }
@@ -54,20 +58,81 @@ public class PlayerTank : TankBase
     private void Fire()
     {
         if (!isFiring || fireElapsedTime < fireDelay) return;
-            poolingScript.ActivateProjectile();
-            fireElapsedTime = 0f;
+        poolingScript.ActivateProjectile();
+        fireElapsedTime = 0f;
     }
 
 
-    private void Move() 
+    private void MovePlayer()
     {
-        transform.Translate(movementX * speed * Time.deltaTime, movementY * speed * Time.deltaTime, 0);
+
+        playerRb.velocity = new Vector2(CalculateSpeed(movementX), CalculateSpeed(movementY));
+        if (movementX > 0)
+        {
+            FaceRight();
+        }
+        if (movementX < -0)
+        {
+            FaceLeft();
+            
+        }
+        if (movementY < 0)
+        {
+            FaceDown();
+        }
+
+        if (movementY > -0)
+        {
+            FaceUp();
+        }
+
+    }
+
+    private void FaceUp()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void FaceDown()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 180);
+    }
+
+    private void FaceLeft()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 90);
+    }
+
+    private void FaceRight()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, -90);
+    }
+
+
+    private void GetPlayerInput()
+    {
+        movementX = Input.GetAxis("Horizontal");
+        movementY = Input.GetAxis("Vertical");
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isFiring = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isFiring = false;
+        }
         
     }
 
-    private void SetRotationBasedOnMovement()
+    private float CalculateSpeed(float movement)
     {
-        facingDirection.z = movementX;
+        return movement * Time.deltaTime * speed;
     }
+
 }
+    
+
+
 
