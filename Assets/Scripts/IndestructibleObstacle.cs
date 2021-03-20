27fa4aad26c2790ebf102725eceabed100c7852a
Mario Alina <Epakfailer@gmail.com>
2021-03-20 12:19:25 +0100

    Fixed Indestructible obstacles

---
+++ Assets/Scripts/IndestructibleObstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Deactivates all lasers
+public class IndestructibleObstacle : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.name.Contains("Laser")) return;
+        collision.gameObject.SetActive(false);
+    }
+}
